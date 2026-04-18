fd516803127e28f1cfb7d3c8e26b63412844a315
ilia <ilikosov@gmail.com>
2026-04-18 22:14:54 +0400

    clear

--- infra/postgres/init.sql
+++
@@ -1,5 +0,0 @@
--- Create extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
--- Create additional indexes (Prisma will create tables)
--- This file can be used for any custom SQL that needs to run on database initialization
--- shared/index.ts
+++
@@ -1,8 +0,0 @@
-// Export types
-export * from './types';
-
-// Export utilities
-export * from './utils';
-
-// Export constants
-export * from './constants';
